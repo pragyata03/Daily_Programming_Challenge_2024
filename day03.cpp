b218710b5df79a4052b1254d09aed7864d72f47f
pragyata03 <132290210+pragyata03@users.noreply.github.com>
2024-09-11 22:03:54 +0530

    Add files via upload

---
+++ day03.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Function for repeating number
+int repeating_number(int n, int arr[])
+{
+    for (int i = 0; i < n; i++) {
+        int j;
+        for (j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j])
+                return arr[i];
+        }
+    }
+    return -1;
+}
+
+//Main Function
+int main()
+{
+    int arr[] = {3, 1, 3, 4, 2};
+    int n = sizeof(arr) / sizeof(int);
+    cout << "The number in the array that is getting repeated: "<< endl<<repeating_number(arr, n);
+    return 0;
+}
